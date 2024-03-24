bd6df6657971371463e148178d9ba7446a7e3b4c
Peter Kim <peterkim.pk1@gmail.com>
2024-03-24 10:04:37 -0400

    feat: 4 functions passing all tests

--- airport/airport.js
+++ airport/airport.js
@@ -1,9 +1,47 @@
 
+function createAirport(airportName, airlines, availableGates) {
+  var createdAirport = {};
+  createdAirport.name = airportName;
+  createdAirport.availableGates = availableGates;
+  createdAirport.airlines = airlines;
+  return createdAirport;
+}
+
+function welcomeGuests(airport) {
+  return `Welcome to ${airport['name']}!`
+}
 
 
+
+
+
+var updatedAirportGates = {};
+
+function landPlanes(airport,gate) {
+  var updatedAirportGates2 = {};
+  if (airport['availableGates'] > gate) {
+    var numberOfAvailableGates = airport['availableGates'] - gate
+    updatedAirportGates.availableGates = numberOfAvailableGates;
+    updatedAirportGates.message = `Success! Current availability is ${numberOfAvailableGates}.`
+    return updatedAirportGates;
+  }
+  
+  if (Object.keys(updatedAirportGates).length !== 0) {
+    if (updatedAirportGates['availableGates'] < gate) {
+      overflow = gate - updatedAirportGates['availableGates']
+      updatedAirportGates2.availableGates = 0;
+      updatedAirportGates2.message = `Oh no! Not enough gates available. Current overflow is ${overflow}.`
+      return updatedAirportGates2;
+    }
+  }
+}
+
+function checkAirlineLocations() {
+  
+}
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
-  // checkAirlineLocations
+  createAirport, 
+  welcomeGuests, 
+  landPlanes, 
+  checkAirlineLocations
 };
--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,44 @@
+var fedCounter = 0;
+function createDragon(dragonName, rider, temperment) {
+  var dragon= {};
+  dragon.name = dragonName;
+  dragon.rider = rider;
+  dragon.temperment = temperment;
+  dragon.timesEaten = 0;
+  dragon.hungry = true;
+  return dragon;
+}
 
+function greetRider(name) {
+  return `Hi, ${name['rider']}!`
+}
+
+function eat(dragon) {
+  hungerCount = {
+    timesEaten: 0,
+    hungry: true,
+  };
+  fedCounter += 1;
+  hungerCount['timesEaten'] = fedCounter;
+  if (fedCounter >= 3) {
+    hungerCount['hungry'] = false;
+  }
+  return hungerCount
+}
+
+function findFireBreathers(allDragons) {
+  var fireBreathers = [];
+  for (i = 0; i < allDragons.length; i++) {
+    if (allDragons[i]['temperment'] === 'aggressive') {
+      fireBreathers.push(allDragons[i])
+    }
+  }
+  return fireBreathers;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
