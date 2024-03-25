889055628cce418884174bfe82bfc3a08b758852
Peter Kim <peterkim.pk1@gmail.com>
2024-03-25 17:16:07 -0400

    Wrote 3 functions to pass 5 tests

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,36 @@
+function createElevator(name, floor, currentFloor, passengers) {
+    var elevator = {
+        building: name,
+        floors: floor,
+        currentFloor: currentFloor,
+        passengers: passengers
+    };
+    return elevator;
+}
 
+function changeFloors(elevator, floor) {
+    if (elevator.currentFloor !== floor) {
+        if(floor > elevator.floors) {
+            return `Floor ${floor} does not exist!`
+        }
+        else {
+            elevator.currentFloor = floor;
+            return `Taking you to floor ${floor}!`
+        }
+    }
+    return `You\'re already on floor ${floor}!`    
+}
 
-module.exports = {  };
+function dropOffPassenger(elevator, passenger) {
+    for (i = 0; i < elevator['passengers'].length; i++) {
+        if (elevator['passengers'][i] === passenger) {
+            elevator['passengers'].splice(i,1) //removes the index at i
+        }
+    }
+    return elevator['passengers'];
+}
+module.exports = { 
+    createElevator,
+    changeFloors,
+    dropOffPassenger
+ };
