7cc73785b8d51a8193b3235bc8115865ea777f9b
Peter Kim <peterkim.pk1@gmail.com>
2024-03-24 23:00:01 -0400

    feat: 3 functions passed 3 tests

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,31 @@
+function createBirthday(name, month, day) {
+    birthday = {};
+    birthday.name = name;
+    birthday.month = month;
+    birthday.day = day;
+    return birthday;
+}
 
+function celebrateBirthday(birthday) {
+    return `Today is ${birthday['month']}/${birthday['day']}! Happy birthday, ${birthday['name']}!`
+}
 
-module.exports = {  };
+function countBirthdays(birthdays, month) {
+    birthMonths = [];
+    counter = 0;
+    for (i = 0; i < birthdays.length; i++) {
+        birthMonths.push(birthdays[i]['month'])
+    }
+    for (i = 0; i < birthMonths.length; i++) {
+        if (birthMonths[i] === month) {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+
+module.exports = {
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+  };
