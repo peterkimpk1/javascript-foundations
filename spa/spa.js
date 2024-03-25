46f03dca74268543f1cd5f3f4647d7ba3c949f87
Peter Kim <peterkim.pk1@gmail.com>
2024-03-25 18:20:37 -0400

    Wrote 5 functions to pass 7 tests

--- spa/spa.js
+++ spa/spa.js
@@ -1,8 +1,51 @@
+function createCustomer(name, bill, bookings) {
+  customer = {
+    name: name,
+    bill: bill,
+    bookings: []
+  };
+  if (bookings !== undefined) {
+    customer.bookings = bookings;
+  }
+  return customer;
+}
 
-module.exports = { 
-  // createCustomer, 
-  // greeting, 
-  // createService,
-  // bookServices, 
-  // applyGiftCard 
+function greeting(customer) {
+  if (customer['bookings'].length !== 0) {
+    return `${customer['name']}! Welcome back to Happy Spa`
+  }
+  return `${customer['name']}! Welcome to Happy Spa`
+}
+
+function createService(service, cost) {
+  var services = {};
+  if (service === undefined && cost === undefined) {
+    return `Please provide service name and cost.`
+  }
+  services.name = service;
+  services.cost = cost;
+  return services;
+}
+
+function bookServices(name, service) {
+  name.bookings.push(service['name']);
+  name.bill += service['cost'];
+  return name;
+}
+
+function applyGiftCard(services, cardAmount) {
+  affordableServices = [];
+  for (i = 0; i < services.length; i++) {
+    if (services[i]['price'] <= cardAmount) {
+      affordableServices.push(services[i]['name'])
+    }
+  }
+  return affordableServices;
+}
+module.exports = {
+  createCustomer,
+  greeting,
+  createService,
+  bookServices, 
+  applyGiftCard 
 }
