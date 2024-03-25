75738107d4cfd4a1349ab747d7c6c33fb24a60db
Peter Kim <peterkim.pk1@gmail.com>
2024-03-24 23:13:46 -0400

    feat: 3 functions passed 4 tests

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,37 @@
+function createEvent(title, month, day) {
+    event = {
+    title: title,
+    month: month,
+    day: day
+    };
+    if (day > 31 || day === 0) {
+        return `Error: ${day} is not a valid day`
+    }
+    return event;
+}
 
+function createCalendar(name, events) {
+    calendar = {};
+    calendar.owner = name;
+    calendar.events = [];
+    for (i = 0; i < events.length; i++) {
+        calendar.events.push(events[i])
+    }
+    return calendar;
+}
 
-module.exports = {  };
+function reportMonthlyEvents(calendar, month) {
+    monthlyEvents = [];
+    for (i = 0; i < calendar.events.length; i++) {
+        if (calendar.events[i]['month'] === month) {
+            monthlyEvents.push(calendar.events[i])
+        }
+    }
+    return monthlyEvents;
+}
+
+module.exports = { 
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents
+ };
