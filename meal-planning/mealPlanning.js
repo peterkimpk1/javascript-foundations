524d3fae7ddb2a534b01b56593190a1e816aa3d6
Peter Kim <peterkim.pk1@gmail.com>
2024-03-24 23:55:38 -0400

    feat: 3 functions passed 8 tests

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,32 @@
+function createMeal(mealType, calories) {
+    meal = {
+        dishes: [],
+        type: mealType,
+        calorieGoal: calories
+    };
+    return meal;
+}
 
-module.exports = { }
+function addDish(meal, food) {
+    var calorieAdjustment = 0;
+    if (meal['calorieGoal'] > food['calories']) {
+        meal['dishes'].push(food);
+        calorieAdjustment = meal['calorieGoal'] - food['calories'];
+        meal['calorieGoal'] = calorieAdjustment;
+    }
+    return meal;
+}
+
+function calculateCalories(meal) {
+    totalCalories = 0;
+    for (i = 0; i < meal.dishes.length; i++) {
+        totalCalories += meal.dishes[i].calories;
+    }
+    return `${meal.type} has a total of ${totalCalories} calories.`
+}
+
+module.exports = {
+    createMeal,
+    addDish,
+    calculateCalories
+}
