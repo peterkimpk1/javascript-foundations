0706b01dc0a7bf2da48fbae03510340ab1151597
Peter Kim <peterkim.pk1@gmail.com>
2024-03-24 23:44:44 -0400

    feat: 4 functions passed 11 tests

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,50 @@
+function createIngredient(name, price) {
+    var ingredients = {
+        name: 'unknown',
+        price: 0.00
+    };
+    if (name !== undefined) {
+     ingredients.name = name;
+    }
+    if (price !== undefined) {
+     ingredients.price = price;
+    }
+    return ingredients;
+}
 
-module.exports = {}
+function createTaco(tacoName, ingredients) {
+    taco = {
+     name: 'custom',
+     ingredients: []
+    };
+    if(tacoName !== undefined) {
+     taco.name = tacoName;
+    }
+    if(ingredients !== undefined) {
+      for (i = 0; i < ingredients.length; i++) {
+        taco.ingredients.push(ingredients[i])
+      }
+    }
+    return taco;
+}
+
+function addIngredientToTaco(tacoName, ingredient) {
+    if (ingredient === undefined) {
+        return tacoName;
+    }
+    tacoName.ingredients.push(ingredient);
+    return tacoName;
+}
+function calculatePrice(tacoName) {
+    totalPrice = 0;
+    for (i = 0; i < tacoName.ingredients.length; i++) {
+      totalPrice += tacoName.ingredients[i]['price']
+    }
+    return totalPrice;
+}
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice
+}
