cabfb065cb8a32c3224c9bfc5bcbfd7f98da07e6
Peter Kim <peterkim.pk1@gmail.com>
2024-03-26 10:33:50 -0400

    Wrote 4 functions to pass 7 tests

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,38 @@
+function createBarber(name, earnings, cuts) {
+    barber = {
+        name: name,
+        earnings: 0,
+        haircuts: [],
+    };
+    if (earnings !== undefined && cuts !== undefined) {
+        barber.earnings = earnings;
+        barber.haircuts = cuts;
+    }
+    return barber;
+}
 
+function giveCompliment(cut) {
+    return `This ${cut['style']} looks great!`
+}
 
-module.exports = {  };
+function cutHair(barber, cut) {
+    barber['haircuts'].push(cut);
+    barber['earnings'] += cut['price'];
+    return barber;
+}
+
+function listStyles(barber, hairLength) {
+    knownStyles = [];
+    for (i = 0; i < barber['haircuts'].length; i++) {
+        if (barber['haircuts'][i]['hairLength'] === hairLength) {
+            knownStyles.push(barber['haircuts'][i]['style']);
+        }
+    }
+    return knownStyles;
+}
+module.exports = {
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+  };
