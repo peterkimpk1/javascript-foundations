0ba6fadf4ddb42ac34c08612169cd10a5f7d3f78
Peter Kim <peterkim.pk1@gmail.com>
2024-03-26 10:43:49 -0400

    Wrote 3 functions to pass 10 tests

--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,36 @@
+function createItemStock(name, quantity, price) {
+    itemStock = {
+        name: 'unknown',
+        quantity: 0,
+        price: 1.00
+    };
+    if (name !== undefined && quantity !== undefined && price !== undefined) {
+        itemStock['name'] = name;
+        itemStock['quantity'] = quantity;
+        itemStock['price'] = price;
+    }
+    return itemStock;
+}
 
-module.exports = {}
+function makePurchase(item, balance) {
+    if (item['price'] > balance) {
+        return `Sorry, you need at least $${item['price']} to make that purchase`
+    }
+    else if (item['quantity'] === 0) {
+        return `Sorry, there are none left`
+    }
+    return `Here are your ${item['name']}`;
+}
+
+function collectChange(change) {
+    total = 0;
+    for (i = 0; i < change.length; i++) {
+        total += change[i];
+    }
+    return total;
+}
+module.exports = {
+    createItemStock,
+    makePurchase,
+    collectChange
+}
