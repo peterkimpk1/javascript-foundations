93a123cd473c8b06b0a3b139385e584094273aa6
Peter Kim <peterkim.pk1@gmail.com>
2024-03-26 10:58:11 -0400

    Wrote 4 functions to pass 8 tests

--- vhs/vhs.js
+++ vhs/vhs.js
@@ -1,3 +1,45 @@
+function createTape(name, readyToPlay) {
+    tape = {
+        title: "",
+        readyToPlay: false,
+    };
+    if (name !== undefined) {
+        tape.title = name;
+    }
+    if (readyToPlay !== undefined) {
+        tape.readyToPlay = readyToPlay;
+    }
+    return tape;
+}
 
+function reset(track) {
+    if (track['readyToPlay'] !== true) {
+        track.readyToPlay = true;
+    }
+    return track;
+}
 
-module.exports = {  }
+function createCollection(tape1, tape2, tape3) {
+    collection = [];
+    if (tape1 !== undefined && tape2 !== undefined && tape3 !== undefined) {
+        collection.push(tape1);
+        collection.push(tape2);
+        collection.push(tape3);
+        return collection;
+    }
+    return `Your collection is empty.`;
+}
+
+function previewCollection(collection) { //parameter is an array with nested objects
+    preview = [];
+    for (i = 0; i < collection.length; i++) {
+        preview.push(collection[i]['title'])
+    }
+    return preview;
+}
+module.exports = { 
+    createTape,
+    reset,
+    createCollection,
+    previewCollection
+ }
