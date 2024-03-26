71ecffd7454c4dce6a52196c55bda63fe34d8be8
Peter Kim <peterkim.pk1@gmail.com>
2024-03-26 16:57:52 -0400

    Wrote 2 functions to pass 5 tests

--- library/library.js
+++ library/library.js
@@ -1,3 +1,32 @@
+function issueCard(name, age) {
+    card = {
+        name: name,
+        age: age,
+        numBooksCheckedOut: 0
+    };
+    if (age > 12) {
+        card.isChild = false;
+    }
+    else if (age < 12) {
+        card.isChild = true;
+    }
+    return card;
+}
 
+function searchByAuthor(collection, name) {
+    searchResults = [];
+    for (i = 0; i < collection.length; i++) {
+        if (collection[i]['author'] === name) {
+            searchResults.push(collection[i]);
+        }
+    }
+    if (searchResults.length !== 0) {
+        return searchResults;
+    }
+    return `No book found for search criteria`
+}
 
-module.exports = { };
+module.exports = { 
+    issueCard,
+    searchByAuthor
+};
