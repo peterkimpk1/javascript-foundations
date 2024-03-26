236e923b5e5b9b3e12ecb201c0d6606683107d8c
Peter Kim <peterkim.pk1@gmail.com>
2024-03-26 13:56:23 -0400

    Wrote 4 functions to pass 9 tests

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,37 @@
+function createFavoriteFood(food) {
+    dish = {};
+    dish.name = food['dish'];
+    dish.ingredients = food['ingredients'];
+    dish.isSpicy = food['isSpicy'];
+    dish.timesOrdered = 0;
+    return dish;
+}
 
+function commentOnSpiciness(dish) {
+    if (dish.isSpicy === true) {
+        return `Wow, this ${dish['name']} is so spicy!`
+    }
+    return `Phew, this ${dish['name']} is not very spicy.`
+}
 
+function orderFood(dish) {
+    dish['timesOrdered'] += 1
+    return dish;
+}
 
-module.exports = { };
+function createShoppingList(dishes) {
+    list = [];
+    for (i = 0; i < dishes.length; i++) {
+        for(j = 0; j < dishes[i]['ingredients'].length; j++) {
+            list.push(dishes[i]['ingredients'][j]);
+        }
+    }
+    return list;
+}
+
+module.exports = { 
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList
+};
