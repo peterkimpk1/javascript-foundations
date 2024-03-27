12d69a50281004631d598d633456110cf0a49c87
Peter Kim <peterkim.pk1@gmail.com>
2024-03-26 20:48:54 -0400

    Wrote 5 functions to pass 8 tests

--- dj/dj.js
+++ dj/dj.js
@@ -1,8 +1,55 @@
+function createSong(name, artist, favorite) {
+  song = {
+    name: name,
+    artist: artist,
+    favorite: false,
+    hasBeenPlayed: false
+  };
+  if (favorite !== undefined) {
+    song.favorite = favorite;
+  }
+  return song;
+}
+
+function playSong(song) {
+  song['hasBeenPlayed'] = true;
+  return song;
+}
+
+function makePlaylist(playlistName, songs) {
+  playlist = {
+    songs: [],
+  };
+  playlist.name = playlistName;
+  for (i = 0; i < songs.length; i++) {
+    playlist['songs'].push(songs[i]);
+  }
+  return playlist;
+}
+
+function addSongToPlaylist(playlist, song) {
+  playlist['songs'].push(song);
+  return playlist;
+}
+
+function playSongs(playlist, favoritesOnly) {
+  for (i = 0; i < playlist['songs'].length; i++) {
+    if (favoritesOnly === 'favorites only') {
+      if (playlist['songs'][i]['favorite'] === true) {
+        playlist['songs'][i]['hasBeenPlayed'] = true;
+      }
+    }
+    if (favoritesOnly === undefined) {
+      playlist['songs'][i]['hasBeenPlayed'] = true;
+    }
+  }
+  return playlist;
+}
 
 module.exports = { 
-  // createSong, 
-  // playSong, 
-  // makePlaylist, 
-  // addSongToPlaylist, 
-  // playSongs
+  createSong, 
+  playSong, 
+  makePlaylist, 
+  addSongToPlaylist, 
+  playSongs
 };
